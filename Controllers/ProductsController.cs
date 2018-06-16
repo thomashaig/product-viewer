4395b1d3f57a1b85102063e355cc386f0282a628
Thomas <thomas.haig@apeirogon.com.au>
2018-06-16 12:07:19 +0930

    Created first route and able to hit 'api/products' route via Postman and return a JSON structure

--- Controllers/ProductsController.cs
+++ Controllers/ProductsController.cs
@@ -6,6 +6,13 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace product_viewer.Controllers {
     public class ProductsController : Controller {
+        [HttpGet("api/products")]
+        public JsonResult GetProducts() {
+            return new JsonResult(new List<object>() {
+                new { id=1, name="Product 1" },
+                new { id=2, name="Product 2"}
+            });
         
+        }
     }
 }
