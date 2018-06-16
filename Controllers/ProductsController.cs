5afe3954204efc9613923dfe8b831e468e8fe75c
Thomas <thomas.haig@apeirogon.com.au>
2018-06-16 12:27:59 +0930

    Limiting results to a single object

--- Controllers/ProductsController.cs
+++ Controllers/ProductsController.cs
@@ -7,8 +7,14 @@ using Microsoft.AspNetCore.Mvc;
 namespace product_viewer.Controllers {
     [Route("api/products")]
     public class ProductsController : Controller {
+        [HttpGet()]
         public JsonResult GetProducts() {
             return new JsonResult(ProductsDataStore.Current.Products);
+        }
+
+        [HttpGet("{id}")]
+        public JsonResult GetProduct(int id) {
+            return new JsonResult(ProductsDataStore.Current.Products.FirstOrDefault(p => p.Id == id));
         }
     }
 }
