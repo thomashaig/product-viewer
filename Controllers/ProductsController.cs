faecaa65de78fe8166fb580605da49dd1bf7f2b1
Thomas <thomas.haig@apeirogon.com.au>
2018-06-16 12:12:18 +0930

    Added basic model for product (will still handcode JSON)

--- Controllers/ProductsController.cs
+++ Controllers/ProductsController.cs
@@ -5,8 +5,8 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 
 namespace product_viewer.Controllers {
+    [HttpGet("api/products")]
     public class ProductsController : Controller {
-        [HttpGet("api/products")]
         public JsonResult GetProducts() {
             return new JsonResult(new List<object>() {
                 new { id=1, name="Product 1" },
