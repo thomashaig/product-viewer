5f5581e98836aad78a606b8e8fcf7695fc7db205
Thomas <thomas.haig@apeirogon.com.au>
2018-06-17 23:03:18 +0930

    Created interface file for repository, but will take break

---
+++ Services/IProductInfoRepository.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using product_viewer.Models;
+using Microsoft.AspNetCore.JsonPatch;
+using Microsoft.Extensions.Logging;
+using product_viewer.Services;
+
+namespace product_viewer.Services {
+    public interface IProductInfoRepository {
+        
+    }
+}
